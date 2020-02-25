522ae7a9dd19cefc9ada71702d1bf6778d299e03
Yaroslav <61201443+yaros1avsv@users.noreply.github.com>
2020-02-26 00:23:57 +0500

    Add files via upload

---
+++ FSapp/FSapp.cpp
@@ -0,0 +1,48 @@
+﻿// FSapp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//123
+
+#include <iostream>
+#include <windows.h>
+#include <Winioctl.h>
+#include <wchar.h>
+#include <algorithm>
+#include "FSdll.h"
+
+using namespace std;
+
+int main()
+{
+	string diskName;
+
+	// TODO: validation 23
+	while (1)
+	{
+		cout << "Enter your drive letter" << endl;
+		diskName = cin.get();
+		if (diskName.length() == 1)
+		{
+			break;
+		}
+	}
+
+	string diskNameFormated = "\\\\.\\" + diskName + ":";
+
+	string SysName = FindFSName(diskName);
+	if (!fsIsSupported(SysName))
+	{
+		cout << SysName << "not NTFS" << endl;
+		cin.get();
+		cin.get();
+		exit(0);
+	}
+
+	BPB _bpb;
+	if (getFsInfo(diskNameFormated, &_bpb))
+	{
+		PrintBootSectInfo(_bpb);
+	}
+	cout << "Press Enter to exit";
+	cin.get();
+	cin.get();
+	exit(0);
+}
